cbdf4b913a36879b4587b018866eba28f81d14e0
Amy <e.brudzisz@gmail.com>
2025-03-31 19:01:24 +0200

    Update items.ts

--- src/types/items.ts
+++ src/types/items.ts
@@ -5,6 +5,36 @@ export enum EArmorType {
     PLATE = "PLATE"
 }
 
+export enum EWeaponType{
+    DAGGER = "DAGGER",
+    FIST = "FIST",
+    AXE1H = "AXE1H",
+    MACE1H = "MACE1H",
+    SWORD1H = "SWORD1H",
+    POLEARM = "POLEARM",
+    STAFF = "STAFF",
+    AXE2H = "AXE2H",
+    MACE2H = "MACE2H",
+    SWORD2H = "SWORD2H",
+    WARGLAIVE = "WARGLAIVE",
+    BOW = "BOW",
+    CROSSBOW = "CROSSBOW",
+    GUN = "GUN",
+    WAND = "WAND"
+}
+
+export enum EOffhandType{
+    OFFHAND = "OFFHAND",
+    SHIELD = "SHIELD"
+}
+
+export enum EMiscType{
+    NECK = "NECK",
+    FINGER = "FINGER",
+    TRINKET = "TRINKET",
+    BACK = "BACK",
+}
+
 export enum EItemSlot {
     HEAD = "HEAD",
     SHOULDER = "SHOULDER",
@@ -48,6 +78,7 @@ export enum EDungeon{
 }
 
 export enum ERaid{
+    NP = "NP",
     LOU = "LOU"
 }
 
@@ -60,6 +91,7 @@ export type TLootSource = EDungeon | TRaidBoss;
 
 export type TWowItem = {
     name: string;
+    itemId: number,
     slot: EItemSlot;
     mainStats: Array<EMainStat>;
     secondaryStats: Array<ESecondaryStat>;
@@ -68,5 +100,20 @@ export type TWowItem = {
 }
 
 export type TWowItemWeapon = TWowItem & {
-    slot: EItemSlot.TWOHAND | EItemSlot.MAINHAND;
+    slot: EItemSlot.TWOHAND | EItemSlot.MAINHAND | EItemSlot.OFFHAND;
+    itemType: EWeaponType
+}
+
+export type TWowItemArmor = TWowItem & {
+    itemType: EArmorType;
+}
+
+export type TWowItemOffhand = TWowItem & {
+    slot: EItemSlot.OFFHAND;
+    itemType: EOffhandType;
+}
+
+export type TWowItemMisc = TWowItem &{
+    slot: EItemSlot.NECK | EItemSlot.FINGER | EItemSlot.FINGER | EItemSlot.BACK;
+    itemType: EMiscType;
 }
