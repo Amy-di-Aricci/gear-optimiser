063dd109fe18b8b91b7bd32d7b2e16425d1c4489
Amy <e.brudzisz@gmail.com>
2025-08-12 22:53:49 +0200

    Added wowhead tooltips, filled missing items in gear store

--- src/store/gear-store.ts
+++ src/store/gear-store.ts
@@ -1,7 +1,8 @@
 import { TWowItem } from '../types/items';
 import gearStoreJson from './gear-store.json';
+import { MANUAL_ITEMS } from './manual_items';
 
-export const GEAR_STORE: Array<TWowItem> = gearStoreJson as Array<TWowItem>;
+export const GEAR_STORE: Array<TWowItem> = [...(gearStoreJson as Array<TWowItem>), ...MANUAL_ITEMS];
 
 // export const GEAR_STORE: Array<TWowItem> = [
 //   {
